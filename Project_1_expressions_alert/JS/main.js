454a40044108bb4d0a9bf882a5eea1d19df3226a
kezia-yifru <yifrukezia@gmail.com>
2022-05-09 11:54:05 -0700

    commit

---
+++ Project_1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+document.write('Hello, World!'); 
+var A="This is a string" //creating a variable 
+document.write(A); //calling variable A
+var B="This is another string" //creating a variable
+window.alert(B) //calling for variable B
+document.write("I told them, \"Even though I won't be here Monday, you should still plan to be prepared for the exam\" but they were still a tad bit unprepared."); 
+
+document.write("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+var C=("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss"); //creating a variable that uses quotes
+document.write(C)
+var Family="The Yifrus", Mom="Karen", Daughter="Kezia", Son="Travis"; //creating a class
+document.write(Mom); //calling for "Mom's name"
+document.write(3+3); //creating an expression
